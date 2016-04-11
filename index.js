f8c61362c6cb0ee44eaa73a7c209e94d9a0da101
Jannis R <mail@jannisr.de>
2016-04-11 18:57:56 +0200

    more flexible rewrite

--- index.js
+++ index.js
@@ -4,57 +4,40 @@ const normalize = require('normalize-for-search')
 
 
 
-const g = { // global patterns
-	  pl:     /\(pl\)/g
-	, berlin: /\((berlin|bln)\)/g
-	, S:      /^s\s/
-	, U:      /^u\s/
-	, SU:     /^s\+u\s/
-	, str:    /str([^\w]|$)/g
-	, bhf:    /bhf([^\w]]$)/g
-	, hbf:    /hbf\./g
+const delim     = /[\s\/\(\)\-,\.\+]+/
+const specChars = /[^\w\s]|_/g
+
+const replace = {
+	  pl:     'polen'
+	, bln:    'berlin'
+	, s:      'sbahn'
+	, u:      'ubahn'
+	, str:    'strasse'
+	, straße: 'strasse'
+	, bhf:    'bahnhof'
+	, hbf:    'hauptbahnhof'
 }
 
-const l = { // local patterns
-	  str: /str\.?$/
-	, bhf: /bhf\.?$/
-	, hbf: /hbf\.?$/
-}
+const str = /(?!^)str$/g
+const strasse = /(?!^)stra(ss|ß)e$/
 
-const delimiter = /[\s\/\(\)\-,\.]+/
-const specialChars = /[^\w\s]|_/g
-const isNotEmpty = (x) => x.length > 0
 
-const tokenize = (station) => {
-	if ('string' !== typeof station || station.length === 0) return []
-	return normalize(station)
 
-	// expand abbreviations
-	.replace(g.pl,     'polen')
-	.replace(g.berlin, 'berlin')
-	.replace(g.S,      'sbahn ')
-	.replace(g.U,      'ubahn ')
-	.replace(g.SU,     'sbahn ubahn ')
-	.replace(g.hbf,    'hauptbahnhof')
-
-	// People often separate station names differently than the canoical title.
-	// Because of this, we create a new token here.
-	.replace(g.str,    ' strasse ')
-	.replace(g.bhf,    ' bahnhof ')
-
-	.split(delimiter)
-	.filter(isNotEmpty)
-
-	.map((token) => token
-		.trim()
-
-		// expand abbreviations
-		.replace(l.str, 'strasse')
-		.replace(l.bhf, 'bahnhof')
-		.replace(l.hbf, 'hauptbahnhof')
-
-		.replace(specialChars, '')
-	)
-}
+const tokenize = (station) =>
+	('string' !== typeof station || station.length === 0)
+	? []
+	: normalize(station)
+	.split(delim)
+	.filter((x) => x.trim().length > 0)
+	.map((x) => replace[x] || x)
+	.reduce((xs, x) => {
+		// expand suffixes
+		if (str.test(x))
+			xs.push(x.replace(str, ''), 'strasse')
+		else if (strasse.test(x))
+			xs.push(x.replace(strasse, ''), 'strasse')
+		else xs.push(x)
+		return xs
+	}, [])
 
 module.exports = tokenize
